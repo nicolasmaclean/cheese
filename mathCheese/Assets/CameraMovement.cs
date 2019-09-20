c932bc1b5baffc0caf272019c5b7b25d0fb6b46b
Lucas Zimmer <lczimmer0103@gmail.com>
2019-09-20 11:30:00 -0500

    Putting the ant into the scene and creating a moveable camera

---
+++ mathCheese/Assets/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey("w"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if(Input.GetKey("s"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if(Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if(Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
