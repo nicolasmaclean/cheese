b1ed68153533e57d4132c2b6de88ac5b338b0066
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-09-21 19:45:38 -0500

    added rotation using arrow keys

--- mathCheese/Assets/CameraMovement.cs
+++ mathCheese/Assets/CameraMovement.cs
@@ -3,11 +3,13 @@
 public class CameraMovement : MonoBehaviour
 {
     public float speed = 20f;
+    public float rotationSpeed = 1f;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
+        Quaternion rot = transform.rotation;
 
         if(Input.GetKey("w"))
         {
@@ -25,7 +27,24 @@ public class CameraMovement : MonoBehaviour
         {
             pos.x += speed * Time.deltaTime;
         }
+        if(Input.GetKey(KeyCode.UpArrow)){
+            float angle = rotationSpeed * Time.deltaTime;
+            rot *= Quaternion.AngleAxis(angle, Vector3.right);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            float angle = rotationSpeed * Time.deltaTime;
+            rot *= Quaternion.AngleAxis(angle, Vector3.left);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            float angle = rotationSpeed * Time.deltaTime;
+            rot *= Quaternion.AngleAxis(angle, Vector3.down);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            float angle = rotationSpeed * Time.deltaTime;
+            rot *= Quaternion.AngleAxis(angle, Vector3.up);
+        }
 
         transform.position = pos;
+        transform.rotation = rot;
     }
 }
