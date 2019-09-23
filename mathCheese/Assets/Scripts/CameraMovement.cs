4b7ad8090cf7aedf7588fcf4381c1e7748d9803c
Lucas Zimmer <lczimmer0103@gmail.com>
2019-09-23 11:08:43 -0500

    Fixed camera movement speed

--- mathCheese/Assets/Scripts/CameraMovement.cs
+++ mathCheese/Assets/Scripts/CameraMovement.cs
@@ -2,14 +2,17 @@
 
 public class CameraMovement : MonoBehaviour
 {
-    public float speed = 20f;
-    public float rotationSpeed = 10f;
+    public static float speed = 20f;
+    public static float rotationSpeed = 10f;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
         Quaternion rot = transform.rotation;
+
+        //speed = 20f;
+        // rotationSpeed = 20f;
 
         if(Input.GetKey("w"))
         {
@@ -27,14 +30,6 @@ public class CameraMovement : MonoBehaviour
         {
             pos.x += speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.UpArrow)){
-            float angle = rotationSpeed * Time.deltaTime;
-            rot *= Quaternion.AngleAxis(angle, Vector3.right);
-        }
-        if(Input.GetKey(KeyCode.DownArrow)){
-            float angle = rotationSpeed * Time.deltaTime;
-            rot *= Quaternion.AngleAxis(angle, Vector3.left);
-        }
         if(Input.GetKey(KeyCode.LeftArrow)){
             float angle = rotationSpeed * Time.deltaTime;
             rot *= Quaternion.AngleAxis(angle, Vector3.down);
@@ -44,7 +39,7 @@ public class CameraMovement : MonoBehaviour
             rot *= Quaternion.AngleAxis(angle, Vector3.up);
         }
 
-        transform.position = pos;
-        transform.rotation = rot;
+        Camera.main.transform.position = pos;
+        Camera.main.transform.rotation = rot;
     }
 }
