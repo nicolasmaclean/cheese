06e2dd941330d05759bbc7b1f6f390dbb0aae94e
Lucas Zimmer <lczimmer0103@gmail.com>
2019-09-26 11:03:56 -0500

    Fixed Camera

--- mathCheese/Assets/Scripts/CameraMovement.cs
+++ mathCheese/Assets/Scripts/CameraMovement.cs
@@ -5,15 +5,18 @@ public class CameraMovement : MonoBehaviour
     public static float speed = 20f;
     public static float rotationSpeed = 10f;
 
+
+    void Start() {
+        Camera.main.transform.position = new Vector3(0f,20f,0f);
+        Camera.main.transform.rotation = Quaternion.Euler(20f,0,0);
+    }
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
         Quaternion rot = transform.rotation;
 
-        //speed = 20f;
-        // rotationSpeed = 20f;
-
+        //Moving the Camera
         if(Input.GetKey("w"))
         {
             pos.z += speed * Time.deltaTime;
@@ -30,14 +33,19 @@ public class CameraMovement : MonoBehaviour
         {
             pos.x += speed * Time.deltaTime;
         }
+
+        //Rotating the Camera
         if(Input.GetKey(KeyCode.LeftArrow)){
             float angle = rotationSpeed * Time.deltaTime;
-            rot *= Quaternion.AngleAxis(angle, Vector3.down);
+            rot = Quaternion.AngleAxis(angle, Vector3.down) * rot;
         }
         if(Input.GetKey(KeyCode.RightArrow)){
             float angle = rotationSpeed * Time.deltaTime;
-            rot *= Quaternion.AngleAxis(angle, Vector3.up);
+            rot = Quaternion.AngleAxis(angle, Vector3.up) * rot;
         }
+
+        //Zooming the Camera
+        Camera.main.fieldOfView += Input.GetAxis("Mouse ScrollWheel") * speed * Time.deltaTime;
 
         Camera.main.transform.position = pos;
         Camera.main.transform.rotation = rot;
