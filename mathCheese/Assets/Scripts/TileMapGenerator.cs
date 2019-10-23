49f9a7d512e27f5bae82215eecdae00dae510b94
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-10-23 13:29:49 -0500

    Tiles!!
    The tile we have can be generated into a grid of a specified size. The performance is garbage, but I disabled shadows and it helped a lot.
    I think the rocks might have to much poly, so I'll fix that next.

---
+++ mathCheese/Assets/Scripts/TileMapGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapGenerator : MonoBehaviour
+{
+    public int mapWidth, mapHeight;
+    public Transform tilePrefab;
+
+    private Transform[,] tiles;
+    private float tileSize;
+    Renderer renderComponent;
+    void Start()
+    {
+        tiles = new Transform[mapHeight, mapWidth];
+        renderComponent = tilePrefab.GetComponent<Renderer>();
+        tileSize = renderComponent.bounds.size.x;
+        buildMap();
+    }
+
+    public void buildMap()
+    {
+        int z, x;
+        Quaternion up = new Quaternion(-1,0,0,1);
+        for(z = 0; z < mapHeight; z++){
+            for(x = 0; x < mapWidth; x++){
+                tiles[z, x] = Instantiate(tilePrefab, new Vector3(x*tileSize, 0, z*tileSize), up);
+            }
+        }
+    }
+}
