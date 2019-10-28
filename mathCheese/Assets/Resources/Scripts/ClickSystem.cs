13b980e987aaff45892b2188f5e747c04e03f9bc
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-10-28 18:06:24 -0500

    seperated the clicking into a click system script placed on an empty

---
+++ mathCheese/Assets/Resources/Scripts/ClickSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSystem : MonoBehaviour
+{
+    //checks the the last 2 clicked items and moves the unit if applicable
+    void clickMoveUnit()
+    {
+        if(MouseOver.lastClicked.Count >= 2) {
+            GameObject cl0 = MouseOver.lastClicked[MouseOver.lastClicked.Count-2];
+            GameObject cl1 = MouseOver.lastClicked[MouseOver.lastClicked.Count-1];
+            if(cl0 != null && cl1 != null) {
+                if(cl0.GetComponent<MouseOver>().goType == MouseOver.GameObjectType.Unit && cl1.GetComponent<MouseOver>().goType == MouseOver.GameObjectType.Tile){
+                    cl0.transform.parent.GetComponent<Unit>().move(cl1.transform.position);
+                    MouseOver.lastClicked = new System.Collections.Generic.List<GameObject>();
+                    MouseOver.lastClicked.Add(null);
+                    Debug.Log("Click System " + MouseOver.lastClicked.Count);
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        clickMoveUnit();
+    }
+}
--- mathCheese/Assets/Resources/Scripts/TileMapGenerator.cs
+++ mathCheese/Assets/Resources/Scripts/TileMapGenerator.cs
@@ -29,24 +29,4 @@ public class TileMapGenerator : MonoBehaviour
         }
 
     }
-
-    public void clickSystem()
-    {
-        if(MouseOver.lastClicked.Count >= 2) {
-            GameObject cl0 = MouseOver.lastClicked[MouseOver.lastClicked.Count-2];
-            GameObject cl1 = MouseOver.lastClicked[MouseOver.lastClicked.Count-1];
-            if(cl0 != null && cl1 != null) {
-                if(cl0.GetComponent<MouseOver>().goType == MouseOver.GameObjectType.Unit && cl1.GetComponent<MouseOver>().goType == MouseOver.GameObjectType.Tile){
-                    cl0.transform.parent.GetComponent<Unit>().move(cl1.transform.position);
-                    MouseOver.lastClicked = new System.Collections.Generic.List<GameObject>();
-                    MouseOver.lastClicked.Add(null);
-                }
-            }
-        }
-    }
-
-    void Update() 
-    {
-        clickSystem();
-    }
 }
