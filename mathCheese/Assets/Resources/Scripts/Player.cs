c1abba558e557a77f79164009cb22ee2b5936eb9
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-10-28 18:36:12 -0500

    ideas/start of turn system

---
+++ mathCheese/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class Player
+{
+    public List<Unit> units;
+
+    public Player()
+    {
+        units = new List<Unit>();
+    }
+
+    public void EndTurn()
+    {
+
+    }
+}
---
+++ mathCheese/Assets/Resources/Scripts/TurnSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour
+{
+    public static List<Player> players;
+    public static Player currentPlayer;
+
+    void Start()
+    {
+        if(players.Count > 0)
+            currentPlayer = players[0];
+    }
+
+    void Update()
+    {
+        
+    }
+}
