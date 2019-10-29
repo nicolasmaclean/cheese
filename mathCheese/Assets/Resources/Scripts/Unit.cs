869570e143b518b67f51173ff350c59018c3ae07
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-10-29 09:45:13 -0500

    made a border that will appear around units on hover and click

--- mathCheese/Assets/Resources/Scripts/Unit.cs
+++ mathCheese/Assets/Resources/Scripts/Unit.cs
@@ -4,9 +4,12 @@ public class Unit : MonoBehaviour
 {
     public Vector2 unitPosition;
     public Transform unitMeshPrefab;
+    public Transform borderPrefab;
+
     Material unitMeshMaterial;
 
     private Transform unitMesh;
+    private Transform borderMesh;
     
     void Start()
     {
@@ -16,17 +19,20 @@ public class Unit : MonoBehaviour
 
     void onHover(GameObject go)
     {
-        go.GetComponent<Renderer>().material.color = Color.green;
+        // go.GetComponent<Renderer>().material.color = Color.green;
+        borderMesh.GetComponent<Renderer>().enabled = true;
     }
 
     void notHover(GameObject go)
     {
         go.GetComponent<Renderer>().sharedMaterial = unitMeshMaterial;
+        borderMesh.GetComponent<Renderer>().enabled = false;
     }
 
     void onClick(GameObject go)
     {
         go.GetComponent<Renderer>().material.color = Color.red;
+        borderMesh.GetComponent<Renderer>().enabled = true;
     }
 
     public void instantiateMesh()
@@ -36,10 +42,15 @@ public class Unit : MonoBehaviour
         unitMesh.parent = gameObject.transform;
         unitMesh.gameObject.AddComponent<MouseOver>();
         unitMesh.gameObject.GetComponent<MouseOver>().instantiate(onHover, notHover, onClick, MouseOver.GameObjectType.Unit);
+
+        borderMesh = Instantiate(borderPrefab, new Vector3(unitPosition.x, 2, unitPosition.y), up);
+        borderMesh.parent = gameObject.transform;
+        borderMesh.GetComponent<Renderer>().enabled = false;
     }
 
     public void move(Vector3 nPos)
     {
         unitMesh.position = nPos;
+        borderMesh.position = new Vector3(nPos.x, nPos.y + 2, nPos.z);
     }
 }
