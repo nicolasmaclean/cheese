f41101c2368cc7229b4587c67f609c7327fe7787
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-10-31 14:28:21 -0500

    the map will now generate in the editor
    no errors if width or height < 0

---
+++ mathCheese/Assets/Resources/Editor/TileMapEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (TileMapGenerator))]
+public class TileMapEditor : Editor
+{
+    public override void OnInspectorGUI() {
+		TileMapGenerator tileGen = (TileMapGenerator)target;
+
+		if (DrawDefaultInspector ()) {
+			if (tileGen.autoUpdate) {
+                for(int i = tileGen.gameObject.transform.childCount-1; i > -1; i--){
+                    DestroyImmediate(tileGen.gameObject.transform.GetChild(i).gameObject);
+                }
+				tileGen.buildMap();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+            for(int i = tileGen.gameObject.transform.childCount-1; i > -1; i--){
+                DestroyImmediate(tileGen.gameObject.transform.GetChild(i).gameObject);
+            }
+			tileGen.buildMap();
+		}
+	}
+}
--- mathCheese/Assets/Resources/Scripts/TileMapGenerator.cs
+++ mathCheese/Assets/Resources/Scripts/TileMapGenerator.cs
@@ -7,26 +7,32 @@ public class TileMapGenerator : MonoBehaviour
     public Transform tileBorderPrefab;
     public static float tileSizeX;
     public static float tileSizeZ;
+    public bool autoUpdate;
 
     private Tile[,] tiles;
 
     void Start()
     {
-        tiles = new Tile[mapHeight, mapWidth];
-        tileSizeX = tilePrefabs[0].transform.Find("Ground").GetComponent<Renderer>().bounds.size.x;
-        tileSizeZ = tilePrefabs[0].transform.Find("Ground").GetComponent<Renderer>().bounds.size.z;
-
+        for(int i = gameObject.transform.childCount-1; i > -1; i--){
+            DestroyImmediate(gameObject.transform.GetChild(i).gameObject);
+        }
         buildMap();
     }
 
     public void buildMap()
     {
-        Quaternion up = new Quaternion(0,0,0,1);
-        int[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, tilePrefabs.Length);
-        
-        for(int z = 0; z < mapHeight; z++){
-            for(int x = 0; x < mapWidth; x++){
-                tiles[z, x] = new Tile(tilePrefabs[noiseMap[z, x]], tileBorderPrefab, new Vector3(x*tileSizeX, 0, z*tileSizeZ), up, gameObject.transform, true);
+        if(mapHeight > 0 && mapWidth > 0){
+            tiles = new Tile[mapHeight, mapWidth];
+            tileSizeX = tilePrefabs[0].transform.Find("Ground").GetComponent<Renderer>().bounds.size.x;
+            tileSizeZ = tilePrefabs[0].transform.Find("Ground").GetComponent<Renderer>().bounds.size.z;
+
+            Quaternion up = new Quaternion(0,0,0,1);
+            int[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, tilePrefabs.Length);
+            
+            for(int z = 0; z < mapHeight; z++){
+                for(int x = 0; x < mapWidth; x++){
+                    tiles[z, x] = new Tile(tilePrefabs[noiseMap[z, x]], tileBorderPrefab, new Vector3(x*tileSizeX, 0, z*tileSizeZ), up, gameObject.transform, true);
+                }
             }
         }
 
