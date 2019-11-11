b4e447f1bcbbdbf9b0a213509cdc8931192a9734
Lucas Zimmer <lczimmer0103@gmail.com>
2019-11-11 11:56:42 -0600

    Menu

---
+++ mathCheese/Assets/Resources/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    
+    public GameObject Main;
+    public GameObject Options;
+
+    public void quitQame() {
+        Application.Quit();
+    }
+
+    public void startGame() {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void play() {
+        Main.gameObject.SetActive(false);
+        Options.gameObject.SetActive(true);
+    }
+}
