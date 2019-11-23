c3642d7f855a5fe24785e06e4ff9589ecd550a60
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-11-23 16:54:47 -0600

    unit class inheritence
    I have made the queen/harvester prefabs have their respective scripts added in the prefab and other stuff

--- mathCheese/Assets/Resources/Scripts/TileMapGenerator.cs
+++ mathCheese/Assets/Resources/Scripts/TileMapGenerator.cs
@@ -3,7 +3,6 @@
 public class TileMapGenerator : MonoBehaviour
 {
     public static int mapWidth, mapHeight;
-    public int mapW, mapH;
     public Transform[] tilePrefabs;
     public static float tileSize;
     public bool autoUpdate;
@@ -23,14 +22,15 @@ public class TileMapGenerator : MonoBehaviour
         if(mapHeight > 0 && mapWidth > 0){
             tiles = new Transform[mapHeight, mapWidth];
             tileSize = tilePrefabs[0].transform.Find("Ground").GetComponent<Renderer>().bounds.size.x;
+            Unit.unitPositions = new bool[mapHeight, mapWidth];
 
             Quaternion up = new Quaternion(0,0,0,1);
             int[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, tilePrefabs.Length);
             
             for(int z = 0; z < mapHeight; z++){
                 for(int x = 0; x < mapWidth; x++){
-                    tiles[z, x] = Instantiate(tilePrefabs[noiseMap[z, x]], new Vector3(x*tileSize, 0, z*tileSize), up);
-                    tiles[z, x].gameObject.AddComponent<Tile>();
+                    tiles[z, x] = Instantiate(tilePrefabs[noiseMap[z, x]], new Vector3(x*tileSize, 0, z*tileSize), up); //move this into the tile instantiation
+                    tiles[z, x].gameObject.AddComponent<Tile>(); //move this to the prefab after inheritence is set up
                     tiles[z, x].GetComponent<Tile>().instantiateTile(new Vector2(x, z), true);
                     tiles[z, x].parent = gameObject.transform;
                 }
--- mathCheese/Assets/Resources/Scripts/Unit.cs
+++ mathCheese/Assets/Resources/Scripts/Unit.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public Vector2 gridPosition;
-    public int moveRange = 2;
+    public int moveRange = 1;
     public ClickSystem.ClickState clickState;
     public bool updated = false;
+    public static bool[,] unitPositions;
 
     Material unitMeshMaterial;
-    Transform borderT;
-    private static bool[,] unitPositions = new bool[TileMapGenerator.mapWidth, TileMapGenerator.mapHeight];
+    GameObject borderT;
 
     public static bool isTileFilled(Vector2 gPos)
     {
-        if(unitPositions[(int)gPos.x, (int)gPos.y])
+        if(unitPositions[(int)gPos.y, (int)gPos.x])
             return true;
         return false;
     }
 
-    public void instantiateUnit(Vector2 gPos, bool mouse)
+    public virtual void instantiateUnit(Vector2 gPos, bool mouse)
     {
-        unitPositions[(int)gPos.x, (int)gPos.y] = true;
+        unitPositions[(int)gPos.y, (int)gPos.x] = true;
         gridPosition = gPos;
-        borderT = gameObject.transform.Find("Border");
+        borderT = gameObject.transform.Find("Border").gameObject;
         unitMeshMaterial = gameObject.GetComponent<Renderer>().sharedMaterial;
 
         borderT.GetComponent<Renderer>().enabled = false;
@@ -35,7 +35,7 @@ public class Unit : MonoBehaviour
     }
 
     void Update() {
-        if(!updated){
+        if(borderT != null && !updated){
             if(clickState == ClickSystem.ClickState.none)
                 noClickState();
             else if(clickState == ClickSystem.ClickState.hover)
@@ -76,10 +76,10 @@ public class Unit : MonoBehaviour
     public void move(Vector2 nPos)
     {
         moveTilesReset(nPos);
-        if(!unitPositions[(int)nPos.x, (int)nPos.y] && Mathf.Abs(nPos.x - gridPosition.x) <= moveRange && Mathf.Abs(nPos.y - gridPosition.y) <= moveRange){
-            unitPositions[(int)gridPosition.x, (int)gridPosition.y] = false; // maybe add a message about move range if not in range
+        if(!unitPositions[(int)nPos.y, (int)nPos.x] && Mathf.Abs(nPos.x - gridPosition.x) <= moveRange && Mathf.Abs(nPos.y - gridPosition.y) <= moveRange){
+            unitPositions[(int)gridPosition.y, (int)gridPosition.x] = false; // maybe add a message about move range if not in range
             gridPosition = nPos;
-            unitPositions[(int)nPos.x, (int)nPos.y] = true;
+            unitPositions[(int)nPos.y, (int)nPos.x] = true;
             gameObject.transform.position = new Vector3(gridPosition.x * TileMapGenerator.tileSize, 0, gridPosition.y * TileMapGenerator.tileSize);
         }
     }
@@ -90,7 +90,7 @@ public class Unit : MonoBehaviour
             for(int x = -moveRange; x <= moveRange; x++){
                 if((int)gridPosition.y + z > -1 && (int)gridPosition.y + z < TileMapGenerator.tiles.GetLength(0) && (int)gridPosition.x + x > -1 && (int)gridPosition.x + x < TileMapGenerator.tiles.GetLength(1)) {
                     TileMapGenerator.tiles[(int)gridPosition.y + z, (int)gridPosition.x + x].GetComponent<Tile>().clickState = ClickSystem.ClickState.none;
-                    TileMapGenerator.tiles[(int)gridPosition.y + z, (int)gridPosition.x + x].GetComponent<Tile>().isInMoveRange = false; //if the unit is on the edge this gives an out of bounds error
+                    TileMapGenerator.tiles[(int)gridPosition.y + z, (int)gridPosition.x + x].GetComponent<Tile>().isInMoveRange = false;
                     TileMapGenerator.tiles[(int)gridPosition.y + z, (int)gridPosition.x + x].GetComponent<Tile>().updated = false;
                 }
             }
