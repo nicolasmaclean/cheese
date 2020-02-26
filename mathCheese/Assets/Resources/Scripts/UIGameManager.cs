10122256c8309630b4154dd121bfda56b8143645
Lucas Zimmer <lczimmer0103@gmail.com>
2020-02-25 21:36:11 -0600

    fixed ant building

--- mathCheese/Assets/Resources/Scripts/UIGameManager.cs
+++ mathCheese/Assets/Resources/Scripts/UIGameManager.cs
@@ -22,16 +22,18 @@ public class UIGameManager : MonoBehaviour
 
     public void buildAnt() 
     {
+        bool placed = false;
         if(TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().larvae > 5)
         {
             Tile c = ClickSystem.clickHistory[ClickSystem.clickHistory.Count-1].GetComponent<Tile>();
             List<Tile> tiles = c.getAdjacentTiles();
             foreach (Tile t in tiles)
             {
-                if(!Unit.isTileFilled(t.gridPosition))
+                if(!Unit.isTileFilled(t.gridPosition) && !placed)
                 {
                     TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().addUnit(Species.unitType[0],t.gridPosition,new Quaternion(-1,0,0,1));
                     TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().larvae -= 5;
+                    placed = true;
                 }
             }
         }
@@ -39,18 +41,20 @@ public class UIGameManager : MonoBehaviour
 
     public void buildSoldier() 
     {
+        bool placed = false;
         if(TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().larvae > 50 && TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().food > 20 && TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().water > 10)
         {
             Tile c = ClickSystem.clickHistory[ClickSystem.clickHistory.Count-1].GetComponent<Tile>();
             List<Tile> tiles = c.getAdjacentTiles();
             foreach (Tile t in tiles)
             {
-                if(!Unit.isTileFilled(t.gridPosition))
+                if(!Unit.isTileFilled(t.gridPosition) && !placed)
                 {
                     TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().addUnit(Species.unitType[1],t.gridPosition,new Quaternion(-1,0,0,1));
                     TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().larvae -= 50;
                     TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().food -= 20;
                     TurnSystem.players[TurnSystem.currentPlayer].GetComponent<Player>().water -= 10;
+                    placed = true;
                 }
             }
         }
